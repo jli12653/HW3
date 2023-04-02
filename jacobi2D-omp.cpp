837a5cb78fb33dcfbd2ca4d19c02b5d366857479
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-04-02 14:18:47 -0400

    1

--- jacobi2D-omp.cpp
+++ jacobi2D-omp.cpp
@@ -25,7 +25,7 @@ void Jacobi(int N, double *u) {
 	
 // #pragma omp parallel
 // {	
-#pragma omp parallel for schedule(static)
+//#pragma omp parallel for schedule(static)
   for (int i = N+3; i <= N*N+3*N; i++){
   		up = i + N + 2;
 		  down = i - N - 2;
