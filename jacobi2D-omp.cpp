1ede74c166d0cd24c624a4473975873f26bea233
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-04-02 12:15:18 -0400

    Update jacobi2D-omp.cpp

--- jacobi2D-omp.cpp
+++ jacobi2D-omp.cpp
@@ -22,7 +22,7 @@ void Jacobi(int N, double *u) {
 // 	uu[i*(N+2)] = uu[i*(N+2) + N+1] =0.0;
 //   }
   	
-  #pragma omp parallel for
+#pragma omp parallel for
   for (int i = 1; i <=N; i++) {
 	for (int j = 1; j <=N; j++) {
 		k = i * (N + 2) + j;
@@ -58,7 +58,7 @@ void Jacobi(int N, double *u) {
 //   printf("=============================================================\n");
 //   printf("u[0] is %d  \n", u[0]);
 
-  #pragma omp parallel for
+#pragma omp parallel for
   for (int i = 1; i <=N; i++) {
 	for (int j = 1; j <=N; j++) {
 		k = i * (N + 2) + j;
