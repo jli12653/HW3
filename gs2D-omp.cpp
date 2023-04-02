ce11f012685d2e6b5da011492b2492c8216da67d
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-04-02 18:29:07 -0400

    1

--- gs2D-omp.cpp
+++ gs2D-omp.cpp
@@ -1,111 +1,151 @@
+// g++ -fopenmp jacobi2D-omp.cpp && ./a.out
+// g++ -std=c++11 -O3 jacobi2D-omp.cpp && ./a.out
+
+
+
+#include <algorithm>
+#include <stdlib.h>
 #include <stdio.h>
-#include "utils.h"
+#include <math.h>
 #include <cmath>
+#include <string.h>
 
+#ifdef _OPENMP
+#include <omp.h>
+#else
+#include "utils.h"
+#endif
 
-void Jacobi( long N, double *u) {
-  double h = 1.0/N;
-  double *uu = (double*) malloc(N * sizeof(double)); // N 
-  
-  for (int i = 0; i < N; i++) {
+// Given that we are using f = 1, so I ingore the term f, just replace it with 1.
+void gs2D(int N, double *u) {
+  double h = 1.0 / ( N + 1 );
+  double hsq = h*h;
+  double *uu = (double*) malloc( (N+2)*(N+2) * sizeof(double)); // (N+2)^2 
 	
-	if (i == 0){ 
-	double U = u[i+1];
-	double UU = 1.0/2 * (h*h +  U);
-	uu[i] = UU;
+// #pragma omp parallel
+// {	
+#pragma omp parallel for
+  for (int i = 1; i <=N; i++) {
+
+	  for (int j = 1; j <=N; j++) {
+      int k = i*(N+2) + j;
+  	
+      // double U_up = u[k + N + 2];
+      // double U_left = u[k - 1];
+      // double U_right = u[k + 1];
+      // double U_down = u[k - N - 2];
+
+    //+ u[up] + u[left] + u[right] + u[down]
+    //+ U_up + U_left + U_right + U_down
+    //u[i + N + 2] + u[i - 1] + u[i + 1] + u[i - N - 2]
+		
+      uu[k] = 0.25*(hsq + u[k + N + 2] + u[k - 1] + u[k + 1] + u[k - N - 2]);
+
 	}
-	else if (i == N-1){ 
-	double U = u[i-1];
-	double UU = 1.0/2 * (h*h +  U);
-	uu[i] = UU;
-	}
-  	else {
-	double U = u[i-1];
-	double UU = u[i+1];
-	double UUU = 1.0/2 * (h*h + U +  UU);
-	uu[i] = UUU;
+}
+
+	
+
+#pragma omp parallel for
+  for (int i = 1; i <=N; i++) {
+	  for (int j = 1; j <=N; j++) {
+      int k = i*(N+2) + j;
+      double U = uu[k];
+      u[k] = U;
 	}  
   }
-
-  for (int i = 0; i < N; i++) {
-	double U = uu[i];
-  	u[i] = U;  
-  }
+  
 
   free(uu);
 }
 
-void  Gauss_Seidel( long N, double *u) {
-  double h = 1.0/N;
-  double *uu = (double*) malloc(N * sizeof(double)); // N 
 
-
-  for (int i = 0; i < N; i++) {
-  	if (i == 0) {double U = u[i+1]; double UU = 1.0/2 * (h*h + U); uu[i]= UU;}
-	else if (i == N-1){double U = uu[i-1]; double UU = 1.0/2 * (h*h + U); uu[i] = UU; }
-	else{double U = uu[i-1]; double UU = u[i+1]; double UUU = 1.0/2 * (h*h + U + UU); uu[i] = UUU;}  
+double residual(int N, double* u){
+  double h = 1.0/(N+1);
+  double invhsq = 1.0/h/h;
+  double r, temp = 0.0;
+	
+#pragma omp parallel for reduction (+:r)
+  for (int i = 1; i <=N; i++) {
+//#pragma omp parallel for 
+    for (int j = 1; j <=N; j++) {
+      int k = i * (N + 2) + j;
+    
+      temp = ((4.0*u[k] - u[k + N + 2] - u[k - N -2] - u[k - 1] - u[k + 1])*invhsq - 1.0);
+      
+      r += temp * temp; 
+	}
   }
 
-  for (int i = 0; i < N; i++) {
-	double U = uu[i];
-  	u[i] = U;  
-  }
-
-
-  free(uu);
+	
+  return sqrt(r);
 }
-
-double residual(long N, double* u){
-	double h = 1.0/N;
-	double r = 0.0;
-	
-	
-	for (int i=0; i<N; i++){
-		if (i == 0) {double U=u[i]; double UU = u[i+1]; r += pow(((2*U-UU)/h/h-1.0),2);}
-		else if (i == N-1) {double U=u[i]; double UU = u[i-1]; r += pow(((2*U-UU)/h/h-1.0),2);}
-		else {double U=u[i]; double UU = u[i+1]; double UUU = u[i-1]; r += pow(((-UUU+2*U-UU)/h/h-1.0),2);}
-          	
-  }	
-	return sqrt(r);
-} 
 
 int main(int argc, char** argv) {
-  const long N = 100000;
+  int k = 0;
+  int N = 10000;
 
-  //printf(" Iteration       Residual\n");
+  printf(" Iteration       Residual\n");
   
     
-  double* u = (double*) malloc(N * sizeof(double)); // N
+  double* u = (double*) malloc((N+2) * (N+2) * sizeof(double)); // N
  
 
   // Initialize u
-  for (long i = 0; i < N; i++) u[i] = 0.0;
+  for (int i = 0; i < (N+2)*(N+2); i++) u[i] = 0.0;
+  
+//   for (int i = 1; i <=N; i++) {
+// 	for (int j = 1; j <=N; j++) {
+// 		k = i * (N + 2) + j;
+// 		u[k] = 0.5;
+// 	}
+//   }
+
+//   printf("=============================================================\n");
+//   for (int i = 0; i <=N+1; i++) {
+// 	for (int j = 0; j <=N+1; j++) {
+// 		k = i * (N + 2) + j;
+// 		printf("%f  ", u[k]);
+// 	}
+// 	printf("\n");
+//   }
+//   printf("=============================================================\n");
+	
   double Res = residual(N , u);
-  double res = 0.0;
-  double Rr = 0.0;
+  double res = Res;
   int iter = 0;
   
   
-  //printf("%10d %10f\n", iter, Res);
+  printf("%10d %10f\n", iter, Res);
 
-  // PART I
-  //while (iter<5000 && Rr <= 1e4){
-  //  	Gauss_Seidel(N, u);
+  // while (iter<5000 && Res/res < 1e4){
+  //  	gs2D(N, u);
+	  
+	
   //  	res = residual(N,u);
-  //  	Rr = Res/res;
   //  	iter++;
-  //       printf("%10d %10f\n", iter, res);
-  //}
+  //   printf("%10d %10f\n", iter, res);
+  // }
 
-  // PART II
-  Timer t;
-  t.tic();
+	
+#ifdef _OPENMP
+  double t = omp_get_wtime();
+#else
+  Timer tt;
+  tt.tic();
+#endif
+
+  iter = 0;
   while (iter<100) {
-    	Gauss_Seidel(N, u);
+    	gs2D(N, u);
     	iter++;
   }
-  double time = t.toc();
-  printf("Time: %10f\n", time);
+#ifdef _OPENMP
+  t = omp_get_wtime() - t;
+#else
+  double t = tt.toc();
+#endif
+  printf("Time: %10f\n", t);
 
   free(u);
 
